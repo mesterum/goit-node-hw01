e55c6647016a2369c00ecd87db284d080c928efb
Mihai Manole <mesterum@users.noreply.github.com>
2024-08-07 18:24:21 +0000

    pasii 2-5

--- src/service/schemas/user.ts
+++ src/service/schemas/user.ts
@@ -1,5 +1,5 @@
-import { getModelForClass, prop } from "@typegoose/typegoose";
-
+import { DocumentType, getModelForClass, prop } from "@typegoose/typegoose";
+import bcrypt from "bcryptjs";
 export class User {
   @prop({ required: [true, 'Password is required'] })
   password!: string;
@@ -12,6 +12,15 @@ export class User {
 
   @prop({ enum: ["starter", "pro", "business"], default: "starter" })
   subscription: "starter" | "pro" | "business" = "starter";
+
+  public async setPassword(this: DocumentType<User>, password: string) {
+    this.password = await bcrypt.hash(password, 10);
+  }
+
+  public async isValidPassword(this: DocumentType<User>, password: string) {
+    return await bcrypt.compare(password, this.password);
+  }
+
 }
 
 export default getModelForClass(User);
